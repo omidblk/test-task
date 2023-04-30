d6e403c68fbd1c889618991869f841f0ad4d20dc
omidblk <omidmokhtari1999@gmail.com>
2023-04-30 22:11:01 +0430

    add font-awesome to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css:['~/assets/scss/main.scss'],
-    modules: ['@nuxtjs/tailwindcss']
-})
+  css: ["~/assets/scss/main.scss",
+        '@fortawesome/fontawesome-svg-core/styles.css'
+    ],
+  modules: ["@nuxtjs/tailwindcss"],
+  plugins: [{ src: "~/plugins/font-awesome.ts" }],
+});
